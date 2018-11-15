b38d4b2488d0ec57477539adfc4a10a848588021
Martin Heller <mheller@testo.de>
2018-11-15 09:31:42 +0100

    Escape slashs in project and branch names

--- waitForJenkins.sh
+++ waitForJenkins.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-echo Waiting for Jenkin\'s job $JENKINS_HOST/job/$CI_PROJECT_NAME/job/$CI_COMMIT_REF_NAME
+JOB_URL = $JENKINS_HOST/job/$(echo $CI_PROJECT_NAME | sed 's/\//%252F/g')/job/$(echo $CI_COMMIT_REF_NAME | sed 's/\//%252F/g')
 
-while [ $(curl --insecure -sS $(curl --insecure -sS $JENKINS_HOST/job/$CI_PROJECT_NAME/job/$CI_COMMIT_REF_NAME/api/json | jq -r .builds[0].url)api/json | jq .building) == "true" ]
+echo Waiting for Jenkin\'s job $JOB_URL
+
+while [ $(curl --insecure -sS $(curl --insecure -sS $JOB_URL/api/json | jq -r .builds[0].url)api/json | jq .building) == "true" ]
 do
 	sleep 3
 done
 
-if [ $(curl --insecure -sS $(curl --insecure -sS $JENKINS_HOST/job/$CI_PROJECT_NAME/job/$CI_COMMIT_REF_NAME/api/json | jq -r .builds[0].url)api/json | jq -r .result) == "SUCCESS" ]
+if [ $(curl --insecure -sS $(curl --insecure -sS $JOB_URL/api/json | jq -r .builds[0].url)api/json | jq -r .result) == "SUCCESS" ]
 then
 	exit 0
 fi
